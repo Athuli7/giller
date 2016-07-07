b2cb75b52c60cea84c8e69f59646866dfe84af65
Athul B Raj <mail@athulraj.com>
2016-07-07 20:22:41 +0530

    Update giller.js

--- giller.js
+++ giller.js
@@ -2,12 +2,26 @@ var port = 9006;
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//Landing
+app.get('/dashboard/assets/:resFN', function (req, res) {
+  res.sendFile(__dirname + 'res/assets/' + req.params['resFN']);
 });
+
+//Resource
+app.get('/dashboard', function (req, res) {
+  res.sendFile(__dirname + 'res/index1.html');
+});
+
+//Dashboard
+app.get('/dashboard', function (req, res) {
+  res.sendFile(__dirname + 'res/index1.html');
+});
+
+//API
 app.all('/api/v1',function(req,res){
   res.send('API')
 });
+
 app.listen(port, function () {
   console.log('Example app listening on port ' + . port . + ' !');
 });
