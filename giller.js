3a0a3b3a438025404134b619a4e28c11cc6245e7
Athul B Raj <mail@athulraj.com>
2016-07-07 20:26:05 +0530

    Update giller.js

--- giller.js
+++ giller.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 
 //Landing
-app.get('/dashboard/assets/:resFN', function (req, res) {
-  res.sendFile(__dirname + 'res/assets/' + req.params['resFN']);
+app.get('/', function (req, res) {
+  res.send('<html><a href="dashboard/">Dashboard</a></html>');
 });
 
 //Resource
-app.get('/dashboard', function (req, res) {
-  res.sendFile(__dirname + 'res/index1.html');
+app.get('/dashboard/assets/:resFN', function (req, res) {
+  res.sendFile(__dirname + 'res/assets/' + req.params['resFN']);
 });
 
 //Dashboard
