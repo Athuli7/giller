20d9cd99c133561c4e4980ff33b9c7199cd5d38b
Athul B Raj <mail@athulraj.com>
2016-07-08 07:14:50 +0530

    Update giller.js

--- giller.js
+++ giller.js
@@ -1,49 +1,9 @@
 var port = 9006;
-//var flatfile = require('flat-file-db');
-//var db = flatfile(__dirname + '/db/assoc.db');
-var express = require('express');
-var app = express();
-//var bodyParser = require('body-parser');
-//app.use(bodyParser.json()); // support json encoded bodies
-//app.use(bodyParser.urlencoded({ extended: true }));
+var http = require('http');
 const spawn = require('child_process').spawn;
-//var util = require("util");
-/*
-//Landing
-app.get('/', function (req, res) {
-  res.rewrite('/dashboard');
-  //res.send('<html><a href="dashboard/">Dashboard</a></html>');
-});
 
-//Resource
-app.get('/dashboard/:resFN', function (req, res) {
-  res.sendFile(__dirname + '/res/' + req.params['resFN']);
-});
-
-//Dashboard
-app.get('/dashboard', function (req, res) {
-  res.sendFile(__dirname + '/res/index.html');
-});
-
-//API
-app.all('/api/v1',function(req,res){
-  res.send('API')
-});
-
-app.all('/api/v1/repo/list',function(req,res){
-  res.send(db.keys());
-});
-app.all('/api/v1/:repoID/create',function(req,res){
-  db.put(req.params['repoID'], req.body.location);
-});
-
-app.all('/api/v1/:repoID/pull',function(req,res){
-  //db search
-  var tmpdir = db.get(res.params['repoID']);
-  //git pull
-*/
-app.all('/*',function(req,res){
-  var dir = req.path;
+function handleRequest(req, res){
+  var dir = req.url;
   console.log(dir);
   const ls = spawn('git', ['pull', 'origin', 'master'], {cwd: dir});
   ls.stdout.on('data', (data) => {
@@ -54,9 +14,12 @@ app.all('/*',function(req,res){
   });
   ls.on('close', (code) => {
     res.end(`child process exited with code ${code}`);
-  });
-});
+  });  
+}
 
-app.listen(port, function () {
+//Create a server
+var server = http.createServer(handleRequest);
+
+server.listen(port, function () {
   console.log('Example app listening on port ' + port + ' !');
 });
